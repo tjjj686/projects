d21d3e6b7bdd4bb114131fba15ff5ec7260979e7
Tong Jiao <tjiao@uw.edu>
2020-06-17 22:25:25 -0700

    hw5

--- seamcarving/src/seamcarving/DijkstraSeamFinder.java
+++ seamcarving/src/seamcarving/DijkstraSeamFinder.java
@@ -5,12 +5,78 @@ import graphs.Graph;
 import graphs.shortestpaths.DijkstraShortestPathFinder;
 import graphs.shortestpaths.ShortestPathFinder;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class DijkstraSeamFinder implements SeamFinder {
     // TODO: replace all 4 references to "Object" on the line below with whatever vertex type
     //  you choose for your graph
-    private final ShortestPathFinder<Graph<Object, Edge<Object>>, Object, Edge<Object>> pathFinder;
+    class MyVertex {
+        private double energy;
+        private int row;
+        private int col;
+
+        MyVertex(double energy) {
+            this.energy = energy;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            MyVertex myVertex = (MyVertex) o;
+            return Double.compare(myVertex.energy, energy) == 0 &&
+                row == myVertex.row &&
+                col == myVertex.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(energy, row, col);
+        }
+    }
+
+    private class MyHorGraph implements Graph<MyVertex, Edge<MyVertex>> {
+        protected final Map<MyVertex, Set<Edge<MyVertex>>> adjacencyList;
+
+        private MyHorGraph() {
+            this.adjacencyList = new HashMap<>();
+        }
+
+        public void buildMyHorGraph(MyVertex[][] myVertex, MyVertex start, MyVertex end) {
+            for (int w = 0; w < myVertex[0].length; w++) {
+                for (int h = 0; h < myVertex.length; h++) {
+                    if (w != myVertex[0].length - 1) {
+                        adjacencyList.put(myVertex[h][w], findHorEdge(myVertex, w, h));
+                    } else {
+                        Set<Edge<MyVertex>> mySet = new HashSet();
+                        mySet.add(new Edge(myVertex[h][w], end, end.energy));
+                        adjacencyList.put(myVertex[h][w], mySet);
+                    }
+                }
+            }
+            adjacencyList.put(start, addHorEdge(myVertex, start));
+
+        }
+
+        @Override
+        public Collection<Edge<MyVertex>> outgoingEdgesFrom(MyVertex vertex) {
+            return Collections.unmodifiableSet(adjacencyList.getOrDefault(vertex, Set.of()));
+        }
+    }
+
+    private final ShortestPathFinder<Graph<MyVertex, Edge<MyVertex>>, MyVertex, Edge<MyVertex>> pathFinder;
 
     public DijkstraSeamFinder() {
         this.pathFinder = createPathFinder();
@@ -24,10 +90,56 @@ public class DijkstraSeamFinder implements SeamFinder {
         return new DijkstraShortestPathFinder<>();
     }
 
+    private MyVertex[][] buildMyVertex(double[][] energies) {
+        MyVertex[][] re = new MyVertex[energies.length][energies[0].length];
+        for (int w = 0; w < energies[0].length; w++) {
+            for (int h = 0; h < energies.length; h++) {
+                re[h][w].energy = energies[h][w];
+                re[h][w].row = h;
+                re[h][w].col = w;
+            }
+        }
+        return re;
+    }
+
+    private Set<Edge<MyVertex>> findHorEdge(MyVertex[][] myVertex, int w, int h) {
+        Set<Edge<MyVertex>> mySet = new HashSet<>();
+        if (h == 0) {
+            mySet.add(new Edge(myVertex[h][w], myVertex[h][w + 1], myVertex[h][w + 1].energy));
+            mySet.add(new Edge(myVertex[h][w], myVertex[h + 1][w + 1], myVertex[h + 1][w + 1].energy));
+        } else if (h == myVertex.length - 1) {
+            mySet.add(new Edge(myVertex[h][w], myVertex[h][w + 1], myVertex[h][w + 1].energy));
+            mySet.add(new Edge(myVertex[h][w], myVertex[h - 1][w + 1], myVertex[h - 1][w + 1].energy));
+        } else {
+            mySet.add(new Edge(myVertex[h][w], myVertex[h][w + 1], myVertex[h][w + 1].energy));
+            mySet.add(new Edge(myVertex[h][w], myVertex[h - 1][w + 1], myVertex[h - 1][w + 1].energy));
+            mySet.add(new Edge(myVertex[h][w], myVertex[h + 1][w + 1], myVertex[h + 1][w + 1].energy));
+        }
+        return mySet;
+    }
+
+    private Set<Edge<MyVertex>> addHorEdge(MyVertex[][] myVertex, MyVertex start) {
+        Set<Edge<MyVertex>> mySet = new HashSet<>();
+        for (int h = 0; h < myVertex.length; h++) {
+            mySet.add(new Edge(start, myVertex[h][0], myVertex[h][0].energy));
+        }
+        return mySet;
+    }
+
     @Override
     public List<Integer> findHorizontalSeam(double[][] energies) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        List<Integer> re = new ArrayList<>();
+        List<Edge<MyVertex>> shortestEdge = new ArrayList<>();
+        //instantiating the graph implementation and choosing the start and end vertices
+        MyHorGraph g = new MyHorGraph();
+        MyVertex start = new MyVertex(0);
+        MyVertex end = new MyVertex(0);
+        g.buildMyHorGraph(buildMyVertex(energies), start, end);
+        shortestEdge = pathFinder.findShortestPath(g, start, end).edges();
+        for (int i = 0; i < shortestEdge.size() - 1; i++) {
+            re.add(shortestEdge.get(0).to().row);
+        }
+        return re;
     }
 
     @Override
--- seamcarving/src/seamcarving/DualGradientEnergyFunction.java
+++ seamcarving/src/seamcarving/DualGradientEnergyFunction.java
@@ -5,7 +5,89 @@ import edu.princeton.cs.algs4.Picture;
 public class DualGradientEnergyFunction implements EnergyFunction {
     @Override
     public double apply(Picture picture, int x, int y) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (x < 0 || y < 0 || x >= picture.width() || y >= picture.height() || picture.width() < 3 || picture.height() < 3) {
+            throw new IndexOutOfBoundsException();
+        }
+        int rx = 0, gx = 0, bx = 0, ry = 0, gy = 0, by = 0;
+        int rxFoward = 0, rxBackward = 0, rxCent = 0, gxFoward = 0, gxBackward = 0, gxCent = 0, bxFoward = 0, bxBackward = 0, bxCent = 0;
+        int ryFoward = 0, ryBackward = 0, ryCent = 0, gyFoward = 0, gyBackward = 0, gyCent = 0, byFoward = 0, byBackward = 0, byCent = 0;
+        if ((x + 2) < picture.width()) {
+            rxFoward = -3 * picture.get(x, y).getRed() + 4 * picture.get(x + 1, y).getRed()
+                - picture.get(x + 2, y).getRed();
+            gxFoward = -3 * picture.get(x, y).getGreen() + 4 * picture.get(x + 1, y).getGreen()
+                - picture.get(x + 2, y).getGreen();
+            bxFoward = -3 * picture.get(x, y).getBlue() + 4 * picture.get(x + 1, y).getBlue()
+                - picture.get(x + 2, y).getBlue();
+        }
+
+        if ((x - 2) >= 0) {
+            rxBackward = -3 * picture.get(x, y).getRed() + 4 * picture.get(x - 1, y).getRed()
+                - picture.get(x - 2, y).getRed();
+            gxBackward = -3 * picture.get(x, y).getGreen() + 4 * picture.get(x - 1, y).getGreen()
+                - picture.get(x - 2, y).getGreen();
+            bxBackward = -3 * picture.get(x, y).getBlue() + 4 * picture.get(x - 1, y).getBlue()
+                - picture.get(x - 2, y).getBlue();
+        }
+
+        if ((y + 2) < picture.height()) {
+            ryFoward = -3 * picture.get(x, y).getRed() + 4 * picture.get(x, y + 1).getRed()
+                - picture.get(x, y + 2).getRed();
+            gyFoward = -3 * picture.get(x, y).getGreen() + 4 * picture.get(x, y + 1).getGreen()
+                - picture.get(x, y + 2).getGreen();
+            byFoward = -3 * picture.get(x, y).getBlue() + 4 * picture.get(x, y + 1).getBlue()
+                - picture.get(x, y + 2).getBlue();
+        }
+        if ((y - 2) >= 0) {
+            ryBackward = -3 * picture.get(x, y).getRed() + 4 * picture.get(x, y - 1).getRed()
+                - picture.get(x, y - 2).getRed();
+            gyBackward = -3 * picture.get(x, y).getGreen() + 4 * picture.get(x, y - 1).getGreen()
+                - picture.get(x, y - 2).getGreen();
+            byBackward = -3 * picture.get(x, y).getBlue() + 4 * picture.get(x, y - 1).getBlue()
+                - picture.get(x, y - 2).getBlue();
+        }
+        if ((x + 1) < picture.width() && (x - 1) >= 0) {
+            rxCent = picture.get(x + 1, y).getRed() - picture.get(x - 1, y).getRed();
+            gxCent = picture.get(x + 1, y).getGreen() - picture.get(x - 1, y).getGreen();
+            bxCent = picture.get(x + 1, y).getBlue() - picture.get(x - 1, y).getBlue();
+        }
+        if ((y + 1) < picture.height() && (y - 1) >= 0) {
+            ryCent = picture.get(x, y + 1).getRed() - picture.get(x, y - 1).getRed();
+            gyCent = picture.get(x, y + 1).getGreen() - picture.get(x, y - 1).getGreen();
+            byCent = picture.get(x, y + 1).getBlue() - picture.get(x, y - 1).getBlue();
+        }
+
+        if (x == 0) {
+            rx = rxFoward;
+            gx = gxFoward;
+            bx = bxFoward;
+        }
+        if (x == picture.width() - 1) {
+            rx = rxBackward;
+            gx = gxBackward;
+            bx = bxBackward;
+        }
+        if (x > 0 && x < picture.width() - 1) {
+            rx = rxCent;
+            gx = gxCent;
+            bx = bxCent;
+        }
+        if (y == 0) {
+            ry = ryFoward;
+            gy = gyFoward;
+            by = byFoward;
+        }
+        if (y == picture.height() - 1) {
+            ry = ryBackward;
+            gy = gyBackward;
+            by = byBackward;
+        }
+        if (y > 0 && y < picture.height() - 1) {
+            ry = ryCent;
+            gy = gyCent;
+            by = byCent;
+        }
+        return Math.sqrt(Math.pow(rx, 2) + Math.pow(gx, 2) + Math.pow(bx, 2) +
+            Math.pow(ry, 2) + Math.pow(gy, 2) + Math.pow(by, 2));
+
     }
 }
